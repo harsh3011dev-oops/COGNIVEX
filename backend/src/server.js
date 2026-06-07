de66dbb1cb0b2f29c45d89d646fc0864951b1fa3
harsh tiwari <harsh3011dev@gmail.com>
2026-06-07 18:52:30 +0530

    fix: cors update

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://magenta-conkies-315dd7.netlify.app"
+  ]
+}));
 app.use(express.json());
 
 // Main entry
